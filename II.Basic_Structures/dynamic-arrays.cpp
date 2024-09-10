65b792600555187194e996fecfdd3578ef3de2ad
Revxz <yosoycris2020@gmail.com>
2024-09-10 22:45:23 +0200

    bug fix to the insertions on the array

--- II.Basic_Structures/dynamic-arrays.cpp
+++ II.Basic_Structures/dynamic-arrays.cpp
@@ -7,8 +7,10 @@ using namespace std;
 vector<int> instArray(5); 
 
 /*In C++, it is recomended to use vectors to work on dynamic arrays*/
-void fillArray(int n, vector<int>& vec);
+void fillArray(vector<int>& vec);
 void listArray(vector<int>& vec);
+void sumArray(vector<int>& vec);
+void insertArray(vector<int>& vec);
 
 int main(){
     /*Create a integer vectors with initial size of 5*/
@@ -18,50 +20,76 @@ int main(){
 
         cout << "--- Dynamic Arrays ---" << endl;
         cout << endl;
-        cout << "1. Fill Array " << endl;
-        cout << "2. Show Array values " << endl;
-        cout << "3. Add all elements of the array " << endl;
-        cout << "4. Calculate the Average of the array " << endl;
-        cout << "5. Calculate maximum and mínimum of the array " << endl;
-        cout << "6. Exit program " << endl;
+        cout << "1. Insert number to the Array " << endl;
+        cout << "2. Fill Array " << endl;
+        cout << "3. Show Array values " << endl;
+        cout << "4. Add all elements of the array " << endl;
+        cout << "5. Calculate the Average of the array " << endl;
+        cout << "6. Calculate maximum and mínimum of the array " << endl;
+        cout << "7. Exit program " << endl;
         cout << endl;
         cout << "What do you want to do? (Type a number option)-> ";
         cin >> response;
 
         switch(response){
             case 1:
-                fillArray(instArray);
+                insertArray(instArray);
                 break;
             case 2:
-                listArray(instArray);
-            break;
+                fillArray(instArray);
+                break;
             
+            case 3:
+                listArray(instArray);
+                break;
+
+            case 4: 
+                sumArray(instArray);
+                break;
+
+            case 7:
+                cout << "Exiting..."<< endl;
+                return 0;
 
             default:
-                cout << "Please type a number" << endl;
+                cout << "ERROR: Number not detected " << endl;
                 break;
+                
         }
-        return 0;        
     }
+        return 0;        
 }
 
-/*Function that fills the vector*/
-void fillArray(vector<int>& vec){
+void insertArray(vector<int>& vec){
     int n;
-    char answer = 'Y';
-    while(toupper(answer) != 'N'){
+    cout << "Insert number to the array -> ";
+    cin >> n;
+    vec.push_back(n); /*push_back is a function to insert the value to the array*/
+}
+
+/*Function that fills the array*/
+void fillArray(vector<int>& vec){
+        int n;
         cout << "Insert number -> ";
         cin >> n;
         for(int i = 0; i < vec.size(); i++){
-            vec[i] = n;
-        }  
-    }
+            vec[i] = n; 
+        }
 }
 
+/*List the content of the array*/
 void listArray(vector<int>& vec){
     cout << "Array values :" << endl;
     for (int i = 0; i < vec.size(); i++) {
         cout << vec[i] << " ";
     }
     cout << endl;
+}
+
+/*Adds all the elements of the array*/
+void sumArray(vector<int>& vec){
+    int sum = 0;
+    for (int i = 0; i < vec.size(); i++){
+        sum += vec[i];
+    }
 }
